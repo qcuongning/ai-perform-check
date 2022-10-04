863580dce5e2994bcb3bda91bedbb4cc2bb3bf0c
qcuongning <nihnquoccuong@gmail.com>
2022-10-04 12:55:26 +0800

    change name whl

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 wget  -O  onnxruntime_gpu-1.9.0-cp36-cp36m-linux_aarch64.whl https://nvidia.box.com/shared/static/w3dezb26wog78rwm2yf2yhh578r5l144.whl
 
-pip3 install onnx190.whl
+pip3 install onnxruntime_gpu-1.9.0-cp36-cp36m-linux_aarch64.whl
 
 wget -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl
 
-pip3 install torch110.whl
+pip3 install torch-1.10.0-cp36-cp36m-linux_aarch64.whl
